0feb6e476940fdb819a2ac86caefb37565e886ae
pauloxuru <73202040+pauloxuru@users.noreply.github.com>
2022-06-28 15:37:47 -0400

    conexion con bbdd

--- JMTASESORIAS/JMT_ASESORIAS/JMT_ASESORIAS/settings.py
+++ JMTASESORIAS/JMT_ASESORIAS/JMT_ASESORIAS/settings.py
@@ -75,10 +75,17 @@ WSGI_APPLICATION = 'JMT_ASESORIAS.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.mysql',
+'NAME': 'JMT',
+'USER': 'root',
+'PASSWORD': '',
+'HOST': 'localhost',
+'PORT': '',
+'OPTIONS': {
+'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+}
+}
 }
 
 
